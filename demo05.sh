77a959e55ab75a36b74d379cc4fbcea66fcb1b8d
Ubuntu <ubuntu@ip-172-31-31-73.ec2.internal>
2025-01-29 20:39:58 +0000

    List files with .sh extension.

---
+++ demo05.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+<< TASK
+Create a script to list all the files in the current directory with the sh extension.
+TASK
+
+echo "Listing all files with .sh extension."
+ls -l *.sh
+
+
